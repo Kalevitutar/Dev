24e5774067bac5bfb9e4622a09c4d0c4cd89e7c1
Kalevitutar <87034747+Kalevitutar@users.noreply.github.com>
2022-10-19 14:14:06 -0400

    Add files via upload

---
+++ kalevtutar-first-react-app-final-version/src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+	<div>
+      <p className="footer-class">
+            © 2022 7DCoders. All rights reserved.
+        </p>
+  </div>
+  );
+}
+export default Footer;
---
+++ kalevtutar-first-react-app-final-version/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import logo from '../Assets/Img/logo.png';
+
+const Header = () => {
+  return (
+	<div className="header-logo">
+    <img src={logo} alt="logo"/>
+  </div>
+  );
+}
+export default Header;
