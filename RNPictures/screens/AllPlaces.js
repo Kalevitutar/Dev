04fa83b645bc7b6a683fa7ec47689e0ab0783d55
Kalevitutar <87034747+Kalevitutar@users.noreply.github.com>
2022-10-19 14:25:21 -0400

    Add files via upload

---
+++ RNPictures/screens/AllPlaces.js
@@ -0,0 +1,30 @@
+import { useIsFocused } from "@react-navigation/native";
+import { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+
+import PlacesList from "../components/Places/PlacesList";
+import { fetchPlaces } from "../util/database";
+
+function AllPlaces({ route }) {
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    async function loadPlaces() {
+      const places = await fetchPlaces();
+      setLoadedPlaces(places);
+    }
+
+    if (isFocused) {
+      loadPlaces();
+      // setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+    }
+  }, [isFocused]);
+
+  return <PlacesList places={loadedPlaces} />;
+}
+
+export default AllPlaces;
+
+const styles = StyleSheet.create({});
